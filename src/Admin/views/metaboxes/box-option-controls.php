07c9722c7015e0661f16bd736604f47b2821bfb2
gelther <lpw.sormano@gmail.com>
2016-04-15 16:37:50 +0200

    Code consistency changes for src/Admin/views/metaboxes/box-option-controls.php

--- src/Admin/views/metaboxes/box-option-controls.php
+++ src/Admin/views/metaboxes/box-option-controls.php
@@ -8,28 +8,28 @@ defined( 'ABSPATH' ) or exit;
 
 /** @var array $rule_options */
 $rule_options = array(
-	'' => __( "Select a condition", 'scroll-triggered-boxes' ),
-	'everywhere' => __( 'everywhere', 'scroll-triggered-boxes' ),
-	'is_page' => __( 'if page is', 'scroll-triggered-boxes' ),
-	'is_single' => __( 'if post is', 'scroll-triggered-boxes' ),
+	''                    => __( 'Select a condition', 'scroll-triggered-boxes' ),
+	'everywhere'          => __( 'everywhere', 'scroll-triggered-boxes' ),
+	'is_page'             => __( 'if page is', 'scroll-triggered-boxes' ),
+	'is_single'           => __( 'if post is', 'scroll-triggered-boxes' ),
 	'is_post_in_category' => __( 'if is post in category', 'scroll-triggered-boxes' ),
-	'is_post_type' => __( 'if post type is', 'scroll-triggered-boxes' ),
-	'is_url' => __( 'if URL is', 'scroll-triggered-boxes' ),
-	'is_referer' => __( 'if referer is', 'scroll-triggered-boxes' ),
+	'is_post_type'        => __( 'if post type is', 'scroll-triggered-boxes' ),
+	'is_url'              => __( 'if URL is', 'scroll-triggered-boxes' ),
+	'is_referer'          => __( 'if referer is', 'scroll-triggered-boxes' ),
 );
 
 $box_positions = array(
-	'bottom-left' => __( 'Bottom Left', 'scroll-triggered-boxes' ),
+	'bottom-left'  => __( 'Bottom Left', 'scroll-triggered-boxes' ),
 	'bottom-right' => __( 'Bottom Right', 'scroll-triggered-boxes' ),
-	'center' => __( 'Center', 'scroll-triggered-boxes' ),
-	'top-left' => __( 'Top Left', 'scroll-triggered-boxes' ),
-	'top-right' => __( 'Top Right', 'scroll-triggered-boxes' ),
-);
+	'center'       => __( 'Center', 'scroll-triggered-boxes' ),
+	'top-left'     => __( 'Top Left', 'scroll-triggered-boxes' ),
+	'top-right'    => __( 'Top Right', 'scroll-triggered-boxes' ),
+); 
 
 ?>
 <table class="form-table">
 	<?php
-	do_action( 'stb_before_box_option_controls', $box, $opts );
+	do_action( 'stb_before_box_option_controls', $box, $opts ); 
 
 	?>
 	<tr>
@@ -48,7 +48,7 @@ $box_positions = array(
 	<tbody id="stb-box-rules">
 	<?php
 	$key = 0;
-	foreach( $opts['rules'] as $rule ) { if( ! array_key_exists( 'condition', $rule ) ) { continue; } ?>
+	foreach ( $opts['rules'] as $rule ) { if ( ! array_key_exists( 'condition', $rule ) ) { continue; } ?>
 		<tr valign="top" class="stb-rule-row stb-rule-row-<?php echo $key; ?>">
 			<th style="text-align: right; font-weight: normal;">
 				<span class="stb-close stb-remove-rule"><span class="dashicons dashicons-dismiss"></span></span>
@@ -56,7 +56,7 @@ $box_positions = array(
 			<td class="stb-sm">
 				<select class="widefat stb-rule-condition" name="stb[rules][<?php echo $key; ?>][condition]">
 					<optgroup label="<?php _e( 'Basic', 'scroll-triggered-boxes' ); ?>">
-						<?php foreach( $rule_options as $value => $label ) {
+						<?php foreach ( $rule_options as $value => $label ) {
 							printf( '<option value="%s" %s>%s</option>', $value, selected( $rule['condition'], $value ), $label );
 						} ?>
 					</optgroup>
@@ -83,7 +83,7 @@ $box_positions = array(
 		<th><label for="stb_position"><?php _e( 'Box Position', 'scroll-triggered-boxes' ); ?></label></th>
 		<td>
 			<select id="stb_position" name="stb[css][position]" class="widefat">
-				<?php foreach( $box_positions as $value => $label ) {
+				<?php foreach ( $box_positions as $value => $label ) {
 					printf( '<option value="%s" %s>%s</option>', $value, selected( $opts['css']['position'], $value ), $label );
 				} ?>
 			</select>
@@ -93,8 +93,8 @@ $box_positions = array(
 	<tr valign="top">
 		<th><label><?php _e( 'Animation', 'scroll-triggered-boxes' ); ?></label></th>
 		<td colspan="3">
-			<label><input type="radio" name="stb[animation]" value="fade" <?php checked($opts['animation'], 'fade'); ?> /> <?php _e( 'Fade In', 'scroll-triggered-boxes' ); ?></label> &nbsp;
-			<label><input type="radio" name="stb[animation]" value="slide" <?php checked($opts['animation'], 'slide'); ?> /> <?php _e( 'Slide In', 'scroll-triggered-boxes' ); ?></label>
+			<label><input type="radio" name="stb[animation]" value="fade" <?php checked( $opts['animation'], 'fade' ); ?> /> <?php _e( 'Fade In', 'scroll-triggered-boxes' ); ?></label> &nbsp;
+			<label><input type="radio" name="stb[animation]" value="slide" <?php checked( $opts['animation'], 'slide' ); ?> /> <?php _e( 'Slide In', 'scroll-triggered-boxes' ); ?></label>
 			<p class="help"><?php _e( 'Which animation type should be used to show the box when triggered?', 'scroll-triggered-boxes' ); ?></p>
 		</td>
 	</tr>
@@ -105,7 +105,7 @@ $box_positions = array(
 			<label><input type="radio" class="stb-auto-show-trigger" name="stb[trigger]" value="" <?php checked( $opts['trigger'], '' ); ?> /> <?php _e( 'Never', 'scroll-triggered-boxes' ); ?></label><br />
 			<label><input type="radio" class="stb-auto-show-trigger" name="stb[trigger]" value="instant" <?php checked( $opts['trigger'], 'instant' ); ?> /> <?php print __( 'Yes, immediately after loading the page.', 'scroll-triggered-boxes' ); ?><input type="number" style="visibility: hidden;" /></label><br />
 			<label><input type="radio" class="stb-auto-show-trigger" name="stb[trigger]" value="percentage" <?php checked( $opts['trigger'], 'percentage' ); ?> /> <?php printf( __( 'Yes, when at %s of page height', 'scroll-triggered-boxes' ), '<input type="number" name="stb[trigger_percentage]" min="0" max="100" value="' . esc_attr( $opts['trigger_percentage'] ) . '" />%' ); ?></label><br />
-			<label><input type="radio" class="stb-auto-show-trigger" name="stb[trigger]" value="element" <?php checked( $opts['trigger'], 'element' ); ?> /> <?php printf( __( 'Yes, when at element %s', 'scroll-triggered-boxes' ), '<input type="text" name="stb[trigger_element]" value="' . esc_attr( $opts['trigger_element'] ) . '" placeholder="' . __( 'Example: #comments', 'scroll-triggered-boxes') .'" />' ); ?></label><br />
+			<label><input type="radio" class="stb-auto-show-trigger" name="stb[trigger]" value="element" <?php checked( $opts['trigger'], 'element' ); ?> /> <?php printf( __( 'Yes, when at element %s', 'scroll-triggered-boxes' ), '<input type="text" name="stb[trigger_element]" value="' . esc_attr( $opts['trigger_element'] ) . '" placeholder="' . __( 'Example: #comments', 'scroll-triggered-boxes' ) . '" />' ); ?></label><br />
 			<?php do_action( 'stb_output_auto_show_trigger_options', $opts ); ?>
 		</td>
 	</tr>
@@ -113,14 +113,14 @@ $box_positions = array(
 	<tr valign="top">
 		<th><label for="stb_cookie"><?php _e( 'Cookie expiration days', 'scroll-triggered-boxes' ); ?></label></th>
 		<td colspan="3">
-			<input type="number" id="stb_cookie" name="stb[cookie]" min="0" step="1" value="<?php echo esc_attr($opts['cookie']); ?>" />
+			<input type="number" id="stb_cookie" name="stb[cookie]" min="0" step="1" value="<?php echo esc_attr( $opts['cookie'] ); ?>" />
 			<p class="help"><?php _e( 'After closing the box, how many days should it stay hidden?', 'scroll-triggered-boxes' ); ?></p>
 		</td>
 	</tr>
 	<tr valign="top">
 		<th><label for="stb_hide_on_screen_size"><?php _e( 'Do not auto-show box on small screens?', 'scroll-triggered-boxes' ); ?></label></th>
 		<td colspan="3">
-			<p><?php printf( __( 'Do not auto-show on screens smaller than %s.', 'scroll-triggered-boxes' ), '<input type="number" min="0" name="stb[hide_on_screen_size]" value="' . esc_attr( $opts['hide_on_screen_size'] ) . '" placeholder="'. esc_attr( $opts['css']['width'] ) .'" style="max-width: 70px;" />px' ); ?></p>
+			<p><?php printf( __( 'Do not auto-show on screens smaller than %s.', 'scroll-triggered-boxes' ), '<input type="number" min="0" name="stb[hide_on_screen_size]" value="' . esc_attr( $opts['hide_on_screen_size'] ) . '" placeholder="' . esc_attr( $opts['css']['width'] ) . '" style="max-width: 70px;" />px' ); ?></p>
 			<p class="help"><?php _e( 'Set to <code>0</code> if you <strong>do</strong> want to auto-show the box on small screens.', 'scroll-triggered-boxes' ); ?></p>
 		</td>
 
@@ -154,7 +154,7 @@ $box_positions = array(
 		<td class="stb-sm">
 			<select class="widefat stb-rule-condition" name="stb[rules][{{{data.key}}}][condition]">
 				<optgroup label="<?php _e( 'Basic', 'scroll-triggered-boxes' ); ?>">
-					<?php foreach( $rule_options as $value => $label ) {
+					<?php foreach ( $rule_options as $value => $label ) {
 						printf( '<option value="%s" %s>%s</option>', $value, '', $label );
 					} ?>
 				</optgroup>
