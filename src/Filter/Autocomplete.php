52b890aef235c1d637fe19eb1526700ba9f4e930
gelther <lpw.sormano@gmail.com>
2016-04-16 14:17:04 +0200

    Code consistency changes for src/Filter/Autocomplete.php

--- src/Filter/Autocomplete.php
+++ src/Filter/Autocomplete.php
@@ -12,16 +12,16 @@ class Autocomplete {
 	 * AJAX listener for autocomplete
 	 */
 	public function ajax() {
-		$q = ( isset( $_GET['q'] ) ) ? sanitize_text_field( $_GET['q'] ) : '';
+		$q    = ( isset( $_GET['q'] ) ) ? sanitize_text_field( $_GET['q'] ) : '';
 		$type = ( isset( $_GET['type'] ) && in_array( $_GET['type'], array( 'page', 'post', 'category', 'post_type' ) ) ) ? $_GET['type'] : 'post';
 
 		// do nothing if supplied 'q' parameter is omitted or empty
 		// or less than 2 characters long
-		if( empty( $q ) || strlen( $q ) < 2 ) {
+		if ( empty( $q ) || strlen( $q ) < 2 ) {
 			die();
 		}
 
-		switch( $type ) {
+		switch ( $type ) {
 
 			default:
 			case 'post':
@@ -42,20 +42,20 @@ class Autocomplete {
 	}
 
 	/**
-	 * @param string $query
-	 * @param string $post_type
+	 * @param  string $query
+	 * @param  string $post_type
 	 *
 	 * @return string
 	 */
 	protected function list_posts( $query, $post_type = 'post' ) {
 		global $wpdb;
-		$sql = $wpdb->prepare( "SELECT p.post_name FROM $wpdb->posts p WHERE p.post_type = '%s' AND p.post_status = 'publish' AND ( p.post_title LIKE '%s' OR p.post_name LIKE '%s' ) GROUP BY p.post_name", $post_type, $query . '%%', $query . '%%' );
+		$sql        = $wpdb->prepare( "SELECT p.post_name FROM $wpdb->posts p WHERE p.post_type = '%s' AND p.post_status = 'publish' AND ( p.post_title LIKE '%s' OR p.post_name LIKE '%s' ) GROUP BY p.post_name", $post_type, $query . '%%', $query . '%%' );
 		$post_slugs = $wpdb->get_col( $sql );
 		return join( $post_slugs, PHP_EOL );
 	}
 
 	/**
-	 * @param string $query
+	 * @param  string $query
 	 *
 	 * @return string
 	 */
@@ -65,16 +65,17 @@ class Autocomplete {
 	}
 
 	/**
-	 * @param string $query
+	 * @param  string $query
 	 *
 	 * @return string
 	 */
 	protected function list_post_types( $query ) {
-		$post_types = get_post_types( array( 'public' => true ), 'names' );
+		$post_types         = get_post_types( array( 'public' => true ), 'names' );
 		$matched_post_types = array_filter( $post_types, function( $name ) use( $query ) {
 			return strpos( $name, $query ) === 0;
-		});
+		} );
 
 		return join( $matched_post_types, PHP_EOL );
 	}
+
 }
